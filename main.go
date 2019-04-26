0b1a624ab9aa824ba028e3737a13a1a9517b508a
Aaron Belovsky <ambelovsky@gmail.com>
2019-04-25 22:27:04 -0500

    Cleanup

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	f "github.com/ambelovsky/gosf"
 )
 
-func init() {
-
-}
+func init() {}
 
 /** ACCESSIBLE METHODS **/
 
@@ -24,31 +22,25 @@ func (a AppMethods) Echo(message string) string {
 type Plugin struct{}
 
 // Activate is an aspect-oriented modular plugin requirement
-func (p Plugin) Activate(app *map[string]interface{}) {
-
-}
+func (p Plugin) Activate(app *map[string]interface{}) {}
 
 // Deactivate is an aspect-oriented modular plugin requirement
-func (p Plugin) Deactivate(app *map[string]interface{}) {
+func (p Plugin) Deactivate(app *map[string]interface{}) {}
 
-}
+// Connect is an aspect-oriented modular plugin requirement
+func (p Plugin) Connect(app *map[string]interface{}) {}
+
+// Disconnect is an aspect-oriented modular plugin requirement
+func (p Plugin) Disconnect(app *map[string]interface{}) {}
 
 // PreReceive is an aspect-oriented modular plugin requirement
-func (p Plugin) PreReceive(clientMessage *f.Message) {
-
-}
+func (p Plugin) PreReceive(clientMessage *f.Message) {}
 
 // PostReceive is an aspect-oriented modular plugin requirement
-func (p Plugin) PostReceive(clientMessage *f.Message) {
-
-}
+func (p Plugin) PostReceive(clientMessage *f.Message) {}
 
 // PreRespond is an aspect-oriented modular plugin requirement
-func (p Plugin) PreRespond(clientMessage *f.Message, serverMessage *f.Message) {
-
-}
+func (p Plugin) PreRespond(clientMessage *f.Message, serverMessage *f.Message) {}
 
 // PostRespond is an aspect-oriented modular plugin requirement
-func (p Plugin) PostRespond(clientMessage *f.Message, serverMessage *f.Message) {
-
-}
+func (p Plugin) PostRespond(clientMessage *f.Message, serverMessage *f.Message) {}
